035a3622d054b8d3bfa333253fec1443f46ea24c
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-06-20 21:25:28 -0500

    Grid Item

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,33 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { GridItem } from "./GridItem";
 
-export const GifGrid = ( { category }) => {
+export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
 
-    useEffect( () => {
-        getGifs();
-    }, []);
+  useEffect(() => {
+    getGifs();
+  }, []);
 
-    const getGifs = async () => {
-        const url = 'https://api.giphy.com/v1/gifs/search?q=porn&limit=10&api_key=vu1XnlfNH8RXUmxg7yoNyH89kRi6ftEc';
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=porn&limit=10&api_key=vu1XnlfNH8RXUmxg7yoNyH89kRi6ftEc";
 
-        const resp = await fetch(url);
-        const { data } = await resp.json();
+    const resp = await fetch(url);
+    const { data } = await resp.json();
 
-        const gifs = data.map(img => {
-            return {
-                id: img.id,
-                title: img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-        console.log(gifs);
-    }
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+    console.log(gifs);
+    setImages(gifs);
+  };
 
-    // getGifs();
+  // getGifs();
 
-
-    return (
-        <div>
-            <h3> { category } </h3>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3> {category} </h3>
+        {images.map( img => (
+          <GridItem key={img.id} {...img} />
+        ))}
+    </div>
+  );
+};
