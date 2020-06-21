28b9e14ee08e427b06b27987f46378edd61ec2e4
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2020-06-21 15:03:37 -0500

    Helpers

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,39 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { GridItem } from "./GridItem";
+import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=porn&limit=10&api_key=vu1XnlfNH8RXUmxg7yoNyH89kRi6ftEc";
-
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    console.log(gifs);
-    setImages(gifs);
-  };
-
-  // getGifs();
+    getGifs(category).then ( imgs => setImages(imgs));
+  }, [ category ]);
 
   return (
-    <div>
+    <>
       <h3> {category} </h3>
-        {images.map( img => (
+      <div className="card-grid">
+        {images.map((img) => (
           <GridItem key={img.id} {...img} />
         ))}
-    </div>
+      </div>
+    </>
   );
 };
